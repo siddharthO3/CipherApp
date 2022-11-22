1d5361c609c80d39bc95e01e194e8c894131d536
siddharth-2003 <guptasiddharth689@gmail.com>
2022-11-22 19:57:51 +0530

    New Release
    VER 1.1

--- app/src/main/java/com/example/cipherapp/MainActivity.kt
+++ app/src/main/java/com/example/cipherapp/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.cipherapp
 
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -12,6 +16,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -19,17 +24,23 @@ import com.example.cipherapp.ui.theme.BrailleTheme
 import com.example.cipherapp.ui.theme.CipherAppThemeWithBackground
 import java.util.*
 
+var clipboardManager: ClipboardManager? = null
+
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+		clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+
 		setContent {
+
 			CipherAppThemeWithBackground {
 				Screen()
 			}
 		}
 	}
 }
+
 
 var page: MutableState<String> = mutableStateOf("0")
 
@@ -69,6 +80,7 @@ fun IntroScreen() {
 	) {
 		Button(onClick = { page.value = "1a" }) {
 			Text(
+				textAlign = TextAlign.Center,
 				text = "Cipher",
 				style = MaterialTheme.typography.button,
 				modifier = Modifier.padding(24.dp)
@@ -76,6 +88,7 @@ fun IntroScreen() {
 		}
 		Button(onClick = { page.value = "1b" }) {
 			Text(
+				textAlign = TextAlign.Center,
 				text = "Decipher",
 				style = MaterialTheme.typography.button,
 				modifier = Modifier.padding(24.dp)
@@ -87,7 +100,7 @@ fun IntroScreen() {
 
 @Composable
 fun NavBar() {
-	TopAppBar(title = { Text(text = "") }, navigationIcon = {
+	TopAppBar(title = { Text(textAlign = TextAlign.Center, text = "") }, navigationIcon = {
 		IconButton(onClick = {
 			when (page.value) {
 				"1a", "1b" -> page.value = "0"
@@ -113,16 +126,40 @@ fun CipherOptions() {
 	) {
 
 		Button(onClick = { page.value = "CC" }, modifier = Modifier.fillMaxWidth()) {
-			Text(text = "Caesar Cipher", style = MaterialTheme.typography.button, fontSize = 32.sp, modifier = Modifier.padding(0.dp, 4.dp))
+			Text(
+				textAlign = TextAlign.Center,
+				text = "Caesar Cipher",
+				style = MaterialTheme.typography.button,
+				fontSize = 32.sp,
+				modifier = Modifier.padding(0.dp, 4.dp)
+			)
 		}
 		Button(onClick = { page.value = "CA" }, modifier = Modifier.fillMaxWidth()) {
-			Text(text = "Atbash Cipher", style = MaterialTheme.typography.button, fontSize = 32.sp, modifier = Modifier.padding(0.dp, 4.dp))
+			Text(
+				textAlign = TextAlign.Center,
+				text = "Atbash Cipher",
+				style = MaterialTheme.typography.button,
+				fontSize = 32.sp,
+				modifier = Modifier.padding(0.dp, 4.dp)
+			)
 		}
 		Button(onClick = { page.value = "CT" }, modifier = Modifier.fillMaxWidth()) {
-			Text(text = "Transposition Cipher", style = MaterialTheme.typography.button, fontSize = 32.sp, modifier = Modifier.padding(0.dp, 4.dp))
+			Text(
+				textAlign = TextAlign.Center,
+				text = "Transposition Cipher",
+				style = MaterialTheme.typography.button,
+				fontSize = 32.sp,
+				modifier = Modifier.padding(0.dp, 4.dp)
+			)
 		}
 		Button(onClick = { page.value = "CP" }, modifier = Modifier.fillMaxWidth()) {
-			Text(text = "Polyalphabetic Substitution Cipher", style = MaterialTheme.typography.button, fontSize = 32.sp, textAlign = TextAlign.Center, modifier = Modifier.padding(0.dp, 4.dp))
+			Text(
+				textAlign = TextAlign.Center,
+				text = "Polyalphabetic Substitution Cipher",
+				style = MaterialTheme.typography.button,
+				fontSize = 32.sp,
+				modifier = Modifier.padding(0.dp, 4.dp)
+			)
 		}
 	}
 }
@@ -138,32 +175,68 @@ fun CipherUI(option: String, text: String, onTextChange: (String) -> Unit) {
 	}
 	var cipher = "$option-$modifiedText"
 
+	var ifTextCopied by remember {
+		mutableStateOf(false)
+	}
+	var onTextCopy: String = if (ifTextCopied) "Cipher copied to clipboard" else ""
+
+
+	val clipData = ClipData.newPlainText("cipher", cipher)
+
 	NavBar()
 	Column(
 		modifier = Modifier
 			.fillMaxSize(),
-		verticalArrangement = Arrangement.SpaceAround,
+		verticalArrangement = Arrangement.SpaceEvenly,
 		horizontalAlignment = Alignment.CenterHorizontally
 	) {
 
-		Text(text = "Cipher", style = MaterialTheme.typography.h2)
+		Text(textAlign = TextAlign.Center, text = "Cipher", style = MaterialTheme.typography.h2)
 
-		Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally){
+		Column(
+			verticalArrangement = Arrangement.Center,
+			horizontalAlignment = Alignment.CenterHorizontally
+		) {
 			TextField(value = text, onValueChange = onTextChange, label = {
-				Text(text = "Enter Text")
+				Text(textAlign = TextAlign.Center, text = "Enter Text")
 			})
 			Spacer(modifier = Modifier.padding(8.dp))
-			SelectionContainer {
-				Text(text = cipher, modifier = Modifier.wrapContentWidth(Alignment.CenterHorizontally))
-			}
+
+			Text(
+				textAlign = TextAlign.Center,
+				text = cipher,
+				modifier = Modifier
+					.wrapContentWidth(Alignment.CenterHorizontally)
+					.clickable {
+						clipboardManager?.setPrimaryClip(clipData)
+						ifTextCopied = true
+					})
+		}
+		if (onTextCopy != null) {
+			Text(
+				text = onTextCopy,
+				fontSize = 16.sp,
+				color = Color.Red,
+				modifier = Modifier.wrapContentWidth(Alignment.CenterHorizontally)
+			)
 		}
 		Spacer(modifier = Modifier.height(32.dp))
 	}
+
 }
+
 
 @Composable
 fun DecipherUI(text: String, onTextChange: (String) -> Unit) {
 	var originalText = decipherText(text)
+
+	val clipData = ClipData.newPlainText("decipher", originalText)
+
+	var ifTextCopied by remember {
+		mutableStateOf(false)
+	}
+	var onTextCopy: String = if (ifTextCopied) "Cipher copied to clipboard" else ""
+
 	NavBar()
 	Column(
 		modifier = Modifier
@@ -173,17 +246,40 @@ fun DecipherUI(text: String, onTextChange: (String) -> Unit) {
 	) {
 
 		BrailleTheme {
-			Text(text = "Decipher", style = MaterialTheme.typography.h2, textAlign = TextAlign.Center)
+			Text(
+				textAlign = TextAlign.Center,
+				text = "Decipher",
+				style = MaterialTheme.typography.h2
+			)
 		}
 
-		Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+		Column(
+			verticalArrangement = Arrangement.Center,
+			horizontalAlignment = Alignment.CenterHorizontally
+		) {
 			TextField(value = text, onValueChange = onTextChange, label = {
-				Text(text = "Enter Coded Text")
+				Text(textAlign = TextAlign.Center, text = "Enter Coded Text")
 			})
 			Spacer(modifier = Modifier.padding(8.dp))
-			SelectionContainer {
-				Text(text = originalText)
-			}
+
+			Text(
+				textAlign = TextAlign.Center,
+				text = originalText,
+				modifier = Modifier
+					.wrapContentWidth(Alignment.CenterHorizontally)
+					.clickable {
+						clipboardManager?.setPrimaryClip(clipData)
+						ifTextCopied = true
+					})
+
+		}
+		if (onTextCopy != null) {
+			Text(
+				text = onTextCopy,
+				fontSize = 16.sp,
+				color = Color.Red,
+				modifier = Modifier.wrapContentWidth(Alignment.CenterHorizontally)
+			)
 		}
 		Spacer(modifier = Modifier.height(32.dp))
 	}
--- app/src/main/java/com/example/cipherapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/cipherapp/ui/theme/Theme.kt
@@ -38,7 +38,7 @@ private val LightColorPalette = lightColors(
 @Composable
 fun CipherAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
-        DarkColorPalette
+        LightColorPalette
     } else {
         LightColorPalette
     }
